5e9f8158d053c6917c587a2a547b8071fcee4306
Michael Mohr <akihana@gmail.com>
2014-09-05 00:27:38 -0700

    Rename brgmodule -> aesmodule

--- python_binding/setup.py
+++ python_binding/setup.py
@@ -3,6 +3,6 @@
 from distutils.core import setup, Extension
 setup(name='aes',
       version='1.0',
-      ext_modules=[Extension('aes', ['brgmodule.c', '../aeskey.c', '../aes_modes.c', '../aestab.c', '../aescrypt.c'],
+      ext_modules=[Extension('aes', ['aesmodule.c', '../aeskey.c', '../aes_modes.c', '../aestab.c', '../aescrypt.c'],
                              include_dirs=['..'], extra_compile_args=['-Wno-sequence-point'])],
      )

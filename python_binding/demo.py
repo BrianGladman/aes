3ace073f2d17225112edd695b854addde0def059
Michael Mohr <akihana@gmail.com>
2014-09-06 22:59:47 -0700

    Print cycle counts in demo.py

--- python_binding/demo.py
+++ python_binding/demo.py
@@ -12,16 +12,18 @@ p.set_cpu_affinity([0])
 random_iv = bytearray(os.urandom(16))
 random_key = bytearray(os.urandom(16))
 
-data = bytearray(range(256))
+data = bytearray(list(range(256)))
 data1 = data[:151]
 data2 = data[151:]
 
+# Note: __PROFILE_AES__ must be defined when building the native
+# module in order for the print statements below to work
 aes_ctr = AES(mode='ctr', key=random_key, iv=random_iv)
-aes_ctr.encrypt(data1)
-aes_ctr.encrypt(data2)
+print('Encrypted data1 in: %5d cycles' % aes_ctr.encrypt(data1))
+print('Encrypted data2 in: %5d cycles' % aes_ctr.encrypt(data2))
 
 data_new = data1 + data2
 aes_ctr = AES(mode='ctr', key=random_key, iv=random_iv)
-aes_ctr.decrypt(data_new)
+print('Decrypted data in:  %5d cycles' % aes_ctr.decrypt(data_new))
 
-print (data == data_new)
+print(data == data_new)

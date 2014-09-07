8ce2e96a6b979b7853a439d3b2407b3ab988e0b5
Brian Gladman <brg@gladman.plus.com>
2014-09-07 17:18:34 +0100

    add Mike's changes to Python code

--- python_binding/demo.py
+++ python_binding/demo.py
@@ -1,19 +1,30 @@
+import os, psutil
 from aes import AES
-from os import urandom
 
-random_iv = bytearray(urandom(16))
-random_key = bytearray(urandom(16))
+# Pin the Python process to the last CPU to measure performance
+# Note: this code works for psutil 1.2.x, not 2.x!
+cpu_count = psutil.NUM_CPUS
+p = psutil.Process(os.getpid())
+proc_list = p.get_cpu_affinity()
+p.set_cpu_affinity([proc_list[-1]])
 
-data = bytearray(range(256))
+# Perform several encryption / decryption operations
+
+random_iv = bytearray(os.urandom(16))
+random_key = bytearray(os.urandom(16))
+
+data = bytearray(list(range(256)))
 data1 = data[:151]
 data2 = data[151:]
 
+# Note: __PROFILE_AES__ must be defined when building the native
+# module in order for the print statements below to work
 aes_ctr = AES(mode='ctr', key=random_key, iv=random_iv)
-aes_ctr.encrypt(data1)
-aes_ctr.encrypt(data2)
+print('Encrypted data1 in: %5d cycles' % aes_ctr.encrypt(data1))
+print('Encrypted data2 in: %5d cycles' % aes_ctr.encrypt(data2))
 
 data_new = data1 + data2
 aes_ctr = AES(mode='ctr', key=random_key, iv=random_iv)
-aes_ctr.decrypt(data_new)
+print('Decrypted data in:  %5d cycles' % aes_ctr.decrypt(data_new))
 
-print (data == data_new)
+print(data == data_new)
